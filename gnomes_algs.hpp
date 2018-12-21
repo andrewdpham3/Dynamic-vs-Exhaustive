9cc388d19fd059d26e7dfcbd22a7cc3d16cd8afd
Andrew Pham <andrewdpham3@gmail.com>
2018-12-21 03:41:34 -0800

    Update gnomes_algs.hpp

--- gnomes_algs.hpp
+++ gnomes_algs.hpp
@@ -36,12 +36,12 @@ path greedy_gnomes_exhaustive(const grid& setting) {
   assert(setting.columns() > 0);
   
   // Compute maximum path length, and check that it is legal.
-  const size_t max_steps = setting.rows() + setting.columns() - 1;
+  const size_t max_steps = setting.rows() + setting.columns() - 2;
   assert(max_steps < 64);
   
   path best(setting);
   std::bitset<63> bits;
-  for(int len=1;len<max_steps;len++){
+  for(int len=0;len<max_steps;len++){
     for(bits=0;bits.to_ulong()<pow(2,len)-1;bits=bits.to_ulong() + 1){
       path candidate(setting);
       for(int k=0;k<len;k++)
